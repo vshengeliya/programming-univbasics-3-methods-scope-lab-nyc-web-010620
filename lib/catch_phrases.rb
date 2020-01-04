fd7afcce47d013ccd537f0221ae07d09e5565919
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 03:45:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,20 +4,20 @@ def mario(phrase)
 end
 
 
-def toadstool
- puts mario (status)
-end
-toadstool
+# def toadstool
+# puts mario (status)
+# end
+# toadstool
 
-def link 
- phrase = "It's Dangerous To Go Alone! take This"
-  puts phrase
-end
-link
+# def link 
+# phrase = "It's Dangerous To Go Alone! take This"
+#   puts phrase
+# end
+# link
 
 
-def any_phrase
-phrase = "Do a Barrel Roll!"
-puts phrase
-end
-any_phrase
+# def any_phrase
+# phrase = "Do a Barrel Roll!"
+# puts phrase
+# end
+# any_phrase
